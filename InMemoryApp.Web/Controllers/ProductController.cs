cfbd4d5ac4e55d2977a9ce108896f60ce08b88c8
Your Name <you@example.com>
2025-10-16 14:45:20 +0300

    Add project files.

---
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Web.Controllers
+{
+
+    public class ProductController : Controller
+    {
+        private IMemoryCache _memoryCache;
+        public ProductController(IMemoryCache memoryCache)
+        {
+                _memoryCache = memoryCache;
+        }
+        public IActionResult Index()
+        {
+            _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            ViewBag.Zaman =  _memoryCache.Get<string>("zaman");
+            return View();
+        }
+    }
+}
